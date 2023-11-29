c753dc7e6ca21cbf4cea5af056298672e384e134
Paulo Pereira <paulo_bas@hotmail.com>
2023-11-29 17:58:30 +0000

    updated gitignore

--- overview_page.py
+++ overview_page.py
@@ -1,11 +1,23 @@
 import dash_bootstrap_components as dbc
-import dash_html_components as html
 import assets
+import historical_data as hd
+from dash import Output, Input, dcc, html, ctx
+import assets.styles as styles
+
+figure_test = hd.figure_for_show
+
 
 content = dbc.Container([
 
         dbc.Row(
-            [dbc.Col(html.H1("Overview"))])],
+            [dbc.Col(html.H1("Overview"))]
+        ),
+
+        
+        dbc.Row(
+            [dcc.Graph(id='december-chart', figure=figure_test)],
+            style=styles.CONTAINER_CARD
+        )],
         style=assets.styles.SPECIFIC_CONTENT_STYLE
 
 
